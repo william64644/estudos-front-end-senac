1dfc669faf7c874a1b3f3a712055132b7923c9a9
William <william.mine64@gmail.com>
2024-08-07 21:58:20 -0300

    atividades

---
+++ javascript/funcoes/saudacao.js
@@ -0,0 +1,8 @@
+let nome = prompt("Digite o seu nome");
+
+function saucadao() {
+  console.log("Seja bem vindo ", nome);
+  document.write("<h1>Seja bem vindo " + nome + "</h1>");
+}
+
+saucadao();
